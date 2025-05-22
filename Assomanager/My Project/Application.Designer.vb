64c3e94bccdacd1c94e992ff080dba6ff3134db4
ELamraniG <137711176+ELamraniG@users.noreply.github.com>
2025-05-22 14:22:20 +0100

    lap

--- Assomanager/My Project/Application.Designer.vb
+++ Assomanager/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Assomanager.Form19
+            Me.MainForm = Global.Assomanager.Form21
         End Sub
     End Class
 End Namespace
