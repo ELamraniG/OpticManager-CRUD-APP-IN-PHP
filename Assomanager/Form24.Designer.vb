424cbca04a35266f3065ea944c965f94f71747e3
ELamraniG <137711176+ELamraniG@users.noreply.github.com>
2025-05-23 12:48:23 +0100

    leaks

---
+++ Assomanager/Form24.Designer.vb
@@ -0,0 +1,177 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form24
+    Inherits System.Windows.Forms.Form
+
+    'Form remplace la méthode Dispose pour nettoyer la liste des composants.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requise par le Concepteur Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'REMARQUE : la procédure suivante est requise par le Concepteur Windows Form
+    'Elle peut être modifiée à l'aide du Concepteur Windows Form.  
+    'Ne la modifiez pas à l'aide de l'éditeur de code.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form24))
+        Me.ImageList1 = New System.Windows.Forms.ImageList(Me.components)
+        Me.idparticipant = New System.Windows.Forms.TextBox()
+        Me.statutpresence = New System.Windows.Forms.ComboBox()
+        Me.idmembre = New System.Windows.Forms.ComboBox()
+        Me.idevenement = New System.Windows.Forms.ComboBox()
+        Me.ligne_modifie = New System.Windows.Forms.Label()
+        Me.type_operation = New System.Windows.Forms.Label()
+        Me.Save = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'ImageList1
+        '
+        Me.ImageList1.ImageStream = CType(resources.GetObject("ImageList1.ImageStream"), System.Windows.Forms.ImageListStreamer)
+        Me.ImageList1.TransparentColor = System.Drawing.Color.Transparent
+        Me.ImageList1.Images.SetKeyName(0, "actualiser.png")
+        Me.ImageList1.Images.SetKeyName(1, "ajouter.png")
+        Me.ImageList1.Images.SetKeyName(2, "imprimer.png")
+        Me.ImageList1.Images.SetKeyName(3, "rechercher.png")
+        Me.ImageList1.Images.SetKeyName(4, "save.png")
+        Me.ImageList1.Images.SetKeyName(5, "supprimer.png")
+        '
+        'idparticipant
+        '
+        Me.idparticipant.Location = New System.Drawing.Point(207, 49)
+        Me.idparticipant.Name = "idparticipant"
+        Me.idparticipant.Size = New System.Drawing.Size(100, 22)
+        Me.idparticipant.TabIndex = 122
+        Me.idparticipant.Visible = False
+        '
+        'statutpresence
+        '
+        Me.statutpresence.FormattingEnabled = True
+        Me.statutpresence.Items.AddRange(New Object() {"inscrit", "présent", "absent"})
+        Me.statutpresence.Location = New System.Drawing.Point(207, 215)
+        Me.statutpresence.Name = "statutpresence"
+        Me.statutpresence.Size = New System.Drawing.Size(200, 24)
+        Me.statutpresence.TabIndex = 117
+        '
+        'idmembre
+        '
+        Me.idmembre.FormattingEnabled = True
+        Me.idmembre.Location = New System.Drawing.Point(207, 165)
+        Me.idmembre.Name = "idmembre"
+        Me.idmembre.Size = New System.Drawing.Size(434, 24)
+        Me.idmembre.TabIndex = 113
+        '
+        'idevenement
+        '
+        Me.idevenement.FormattingEnabled = True
+        Me.idevenement.Location = New System.Drawing.Point(207, 115)
+        Me.idevenement.Name = "idevenement"
+        Me.idevenement.Size = New System.Drawing.Size(434, 24)
+        Me.idevenement.TabIndex = 109
+        '
+        'ligne_modifie
+        '
+        Me.ligne_modifie.AutoSize = True
+        Me.ligne_modifie.Location = New System.Drawing.Point(421, 49)
+        Me.ligne_modifie.Name = "ligne_modifie"
+        Me.ligne_modifie.Size = New System.Drawing.Size(91, 17)
+        Me.ligne_modifie.TabIndex = 114
+        Me.ligne_modifie.Text = "ligne_modifie"
+        Me.ligne_modifie.Visible = False
+        '
+        'type_operation
+        '
+        Me.type_operation.AutoSize = True
+        Me.type_operation.Location = New System.Drawing.Point(518, 49)
+        Me.type_operation.Name = "type_operation"
+        Me.type_operation.Size = New System.Drawing.Size(103, 17)
+        Me.type_operation.TabIndex = 112
+        Me.type_operation.Text = "type_operation"
+        Me.type_operation.Visible = False
+        '
+        'Save
+        '
+        Me.Save.FlatAppearance.BorderSize = 0
+        Me.Save.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.Save.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Save.ImageIndex = 4
+        Me.Save.ImageList = Me.ImageList1
+        Me.Save.Location = New System.Drawing.Point(207, 270)
+        Me.Save.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.Save.Name = "Save"
+        Me.Save.Size = New System.Drawing.Size(51, 37)
+        Me.Save.TabIndex = 119
+        Me.Save.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(207, 95)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(78, 17)
+        Me.Label1.TabIndex = 108
+        Me.Label1.Text = "Événement"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(207, 145)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(58, 17)
+        Me.Label2.TabIndex = 110
+        Me.Label2.Text = "Membre"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(207, 195)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(113, 17)
+        Me.Label3.TabIndex = 116
+        Me.Label3.Text = "Statut Présence"
+        '
+        'Form24
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(679, 347)
+        Me.Controls.Add(Me.idparticipant)
+        Me.Controls.Add(Me.statutpresence)
+        Me.Controls.Add(Me.idmembre)
+        Me.Controls.Add(Me.idevenement)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.ligne_modifie)
+        Me.Controls.Add(Me.type_operation)
+        Me.Controls.Add(Me.Save)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form24"
+        Me.Text = "Gestion des participants"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents ImageList1 As System.Windows.Forms.ImageList
+    Friend WithEvents idparticipant As System.Windows.Forms.TextBox
+    Friend WithEvents statutpresence As System.Windows.Forms.ComboBox
+    Friend WithEvents idmembre As System.Windows.Forms.ComboBox
+    Friend WithEvents idevenement As System.Windows.Forms.ComboBox
+    Friend WithEvents ligne_modifie As System.Windows.Forms.Label
+    Friend WithEvents type_operation As System.Windows.Forms.Label
+    Friend WithEvents Save As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+End Class
---
+++ Assomanager/Form24.vb
@@ -0,0 +1,212 @@
+﻿Imports System.Data.OleDb
+Imports System.IO
+
+Public Class Form24
+
+    Private Sub Form24_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Dim icone As New Icon("images/emp.ico") 'il faut placer emp.ico dans le dossier debug
+        Me.Icon = icone
+        Me.Text = "Formulaire Participant"
+        Me.MinimizeBox = False
+        Me.MaximizeBox = False
+        Me.CenterToScreen()
+
+        Try
+            ' Load events into the dropdown
+            connexion()
+            requete = "SELECT idevenement, titre FROM evenements ORDER BY datedebut DESC"
+            cmdsql()
+
+            Dim data As IDataReader = Nothing
+            Try
+                idevenement.Items.Clear()
+                data = cmd.ExecuteReader()
+
+                While data.Read()
+                    Dim eventId As String = data(0).ToString()
+                    Dim eventTitle As String = data(1).ToString()
+                    idevenement.Items.Add(eventId + " | " + eventTitle)
+                End While
+            Finally
+                ' Always close the reader
+                If data IsNot Nothing AndAlso Not data.IsClosed Then
+                    data.Close()
+                End If
+                deconnexion()
+            End Try
+
+            ' Load members into the dropdown
+            connexion()
+            requete = "SELECT idmembre, nom, prenom FROM membres WHERE statut = 'actif' ORDER BY nom, prenom"
+            cmdsql()
+
+            Try
+                idmembre.Items.Clear()
+                data = cmd.ExecuteReader()
+
+                While data.Read()
+                    Dim membreId As String = data(0).ToString()
+                    Dim membreName As String = data(1).ToString() & " " & data(2).ToString()
+                    idmembre.Items.Add(membreId + " | " + membreName)
+                End While
+            Finally
+                ' Always close the reader
+                If data IsNot Nothing AndAlso Not data.IsClosed Then
+                    data.Close()
+                End If
+                deconnexion()
+            End Try
+
+            ' Set default values for a new participant
+            If type_operation.Text = "Ajouter" Then
+                idparticipant.Text = ""
+                idevenement.SelectedIndex = -1
+                idmembre.SelectedIndex = -1
+                statutpresence.SelectedIndex = 0 ' Default to "inscrit"
+            End If
+
+        Catch ex As Exception
+            MsgBox("Erreur: " & ex.Message, vbExclamation, "Erreur de base de données")
+            deconnexion()
+        End Try
+    End Sub
+
+    Private Sub Save_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Save.Click
+        ' Basic validation
+        If idevenement.Text = "" Then
+            MsgBox("Veuillez sélectionner un événement!", vbExclamation, "Message")
+            idevenement.Focus()
+            Exit Sub
+        End If
+
+        If idmembre.Text = "" Then
+            MsgBox("Veuillez sélectionner un membre!", vbExclamation, "Message")
+            idmembre.Focus()
+            Exit Sub
+        End If
+
+        If statutpresence.Text = "" Then
+            MsgBox("Veuillez sélectionner un statut de présence!", vbExclamation, "Message")
+            statutpresence.Focus()
+            Exit Sub
+        End If
+
+        Try
+            ' Extract the event ID from the dropdown selection
+            Dim eventId As String = ""
+            If idevenement.Text.Contains("|") Then
+                eventId = idevenement.Text.Substring(0, idevenement.Text.IndexOf("|")).Trim()
+            Else
+                eventId = idevenement.Text.Trim()
+            End If
+
+            ' Extract the member ID from the dropdown selection
+            Dim membreId As String = ""
+            If idmembre.Text.Contains("|") Then
+                membreId = idmembre.Text.Substring(0, idmembre.Text.IndexOf("|")).Trim()
+            Else
+                membreId = idmembre.Text.Trim()
+            End If
+
+            ' If empty, show error
+            If String.IsNullOrEmpty(eventId) Then
+                MsgBox("Veuillez sélectionner un événement valide!", vbExclamation, "Message")
+                idevenement.Focus()
+                Exit Sub
+            End If
+
+            If String.IsNullOrEmpty(membreId) Then
+                MsgBox("Veuillez sélectionner un membre valide!", vbExclamation, "Message")
+                idmembre.Focus()
+                Exit Sub
+            End If
+
+            Try
+                connexion()
+
+                If type_operation.Text = "Ajouter" Then
+                    ' Check if this participant already exists for this event
+                    requete = "SELECT COUNT(*) FROM participantevenements WHERE idevenement = ? AND idmembre = ?"
+                    cmdsql()
+                    cmd.Parameters.Clear()
+                    cmd.Parameters.Add("@p1", OleDbType.Integer).Value = Convert.ToInt32(eventId)
+                    cmd.Parameters.Add("@p2", OleDbType.Integer).Value = Convert.ToInt32(membreId)
+                    
+                    Dim count As Integer = Convert.ToInt32(cmd.ExecuteScalar())
+                    
+                    If count > 0 Then
+                        MsgBox("Ce membre est déjà inscrit à cet événement!", vbExclamation, "Message")
+                        deconnexion()
+                        Exit Sub
+                    End If
+
+                    requete = "INSERT INTO participantevenements ([idevenement], [idmembre], [statutpresence]) VALUES (?,?,?)"
+                    cmdsql()
+                    cmd.Parameters.Clear()
+
+                    cmd.Parameters.Add("@p1", OleDbType.Integer).Value = Convert.ToInt32(eventId)
+                    cmd.Parameters.Add("@p2", OleDbType.Integer).Value = Convert.ToInt32(membreId)
+                    cmd.Parameters.Add("@p3", OleDbType.VarChar).Value = statutpresence.Text
+                Else ' Modifier
+                    requete = "UPDATE participantevenements SET [idevenement]=?, [idmembre]=?, [statutpresence]=? WHERE [idparticipantevenement]=?"
+                    cmdsql()
+                    cmd.Parameters.Clear()
+
+                    cmd.Parameters.Add("@p1", OleDbType.Integer).Value = Convert.ToInt32(eventId)
+                    cmd.Parameters.Add("@p2", OleDbType.Integer).Value = Convert.ToInt32(membreId)
+                    cmd.Parameters.Add("@p3", OleDbType.VarChar).Value = statutpresence.Text
+                    cmd.Parameters.Add("@p4", OleDbType.Integer).Value = Convert.ToInt32(idparticipant.Text)
+                End If
+
+                cmd.ExecuteNonQuery()
+            Finally
+                ' Always ensure we close the connection
+                deconnexion()
+            End Try
+
+            Form23.afficher_participants()
+            Me.Close()
+        Catch ex As Exception
+            MsgBox("Erreur: " & ex.Message, vbExclamation, "Erreur SQL")
+            deconnexion() ' Redundant but safe
+        End Try
+    End Sub
+
+    ' Select an event by ID
+    Public Sub SelectEventById(ByVal eventId As String)
+        If String.IsNullOrEmpty(eventId) Then
+            Return
+        End If
+
+        ' Look for the item that starts with the event ID
+        For i As Integer = 0 To idevenement.Items.Count - 1
+            Dim item As String = idevenement.Items(i).ToString()
+            If item.StartsWith(eventId & " |") Then
+                idevenement.SelectedIndex = i
+                Return
+            End If
+        Next
+
+        ' If not found, just display the ID
+        idevenement.Text = eventId
+    End Sub
+
+    ' Select a member by ID
+    Public Sub SelectMembreById(ByVal membreId As String)
+        If String.IsNullOrEmpty(membreId) Then
+            Return
+        End If
+
+        ' Look for the item that starts with the member ID
+        For i As Integer = 0 To idmembre.Items.Count - 1
+            Dim item As String = idmembre.Items(i).ToString()
+            If item.StartsWith(membreId & " |") Then
+                idmembre.SelectedIndex = i
+                Return
+            End If
+        Next
+
+        ' If not found, just display the ID
+        idmembre.Text = membreId
+    End Sub
+End Class
