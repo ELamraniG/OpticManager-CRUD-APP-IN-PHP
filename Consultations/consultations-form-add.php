8845e5372af4fda4c9672415280bb3d806f0e3ef
ELamraniG <moha.mati@hotmail.com>
2025-05-26 09:47:00 +0100

    add consultation

---
+++ Consultations/consultations-form-add.php
@@ -0,0 +1,46 @@
+<?php
+require("../head.php");
+require("../connexion.php");
+$r_list_idpatient = "select `idpatient` as value, CONCAT(nom, ' ', prenom) as label from `patients` order by label";
+$list_idpatient = mysqli_query($con, $r_list_idpatient);
+?>
+<div class="container form-box">
+    <div class="card shadow">
+        <div class="card-body">
+            <h2 class="mb-4">Ajouter - Consultations</h2>
+            <form method="post" action="consultations-add.php">
+                <div class="row">
+        <div class="col-md-6 mb-3">
+            <label class="form-label">Patient</label>
+            <select name="idpatient" class="form-select" required>
+                <?php while ($x = mysqli_fetch_assoc($list_idpatient)) { ?>
+                    <option value="<?php echo e($x['value']); ?>" <?php if ((string)'' == (string)$x['value']) echo "selected"; ?>>
+                        <?php echo e($x['label']); ?>
+                    </option>
+                <?php } ?>
+            </select>
+        </div>
+        <div class="col-md-6 mb-3">
+            <label class="form-label">Date</label>
+            <input type="date" name="dateconsultation" class="form-control" value="<?php echo e(''); ?>" required>
+        </div>
+        <div class="col-md-6 mb-3">
+            <label class="form-label">Motif</label>
+            <textarea name="motif" class="form-control" required><?php echo e(''); ?></textarea>
+        </div>
+        <div class="col-md-6 mb-3">
+            <label class="form-label">Observations</label>
+            <textarea name="observations" class="form-control"><?php echo e(''); ?></textarea>
+        </div>
+        <div class="col-md-6 mb-3">
+            <label class="form-label">Prescription</label>
+            <input type="text" name="prescriptionpdf" class="form-control" value="<?php echo e(''); ?>">
+        </div>
+                </div>
+                <button class="btn btn-primary" type="submit">Enregistrer</button>
+                <a href="consultations-list.php" class="btn btn-secondary">Retour</a>
+            </form>
+        </div>
+    </div>
+</div>
+<?php require("../footer.php"); ?>
