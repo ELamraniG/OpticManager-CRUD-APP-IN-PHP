8f89806c48d055b4962f1188896d64098bfed355
ELamraniG <moha.mati@hotmail.com>
2025-05-27 17:37:00 +0100

    prescription edit

---
+++ Ordonnances/ordonnances-update.php
@@ -0,0 +1,26 @@
+<?php
+require("../connexion.php");
+require("../fonctions.php");
+$id = isset($_POST['id']) ? mysqli_real_escape_string($con, $_POST['id']) : '';
+
+$idconsultation = isset($_POST['idconsultation']) ? mysqli_real_escape_string($con, $_POST['idconsultation']) : '';
+$oeil = isset($_POST['oeil']) ? mysqli_real_escape_string($con, $_POST['oeil']) : '';
+$sphere = isset($_POST['sphere']) ? mysqli_real_escape_string($con, $_POST['sphere']) : '';
+$cylindre = isset($_POST['cylindre']) ? mysqli_real_escape_string($con, $_POST['cylindre']) : '';
+$axe = isset($_POST['axe']) ? mysqli_real_escape_string($con, $_POST['axe']) : '';
+$addition = isset($_POST['addition']) ? mysqli_real_escape_string($con, $_POST['addition']) : '';
+$typecorrection = isset($_POST['typecorrection']) ? mysqli_real_escape_string($con, $_POST['typecorrection']) : '';
+
+$parties = array();
+$parties[] = "`idconsultation` = '" . $idconsultation . "'";
+$parties[] = "`oeil` = '" . $oeil . "'";
+$parties[] = "`sphere` = '" . $sphere . "'";
+$parties[] = "`cylindre` = '" . $cylindre . "'";
+$parties[] = "`axe` = '" . $axe . "'";
+$parties[] = "`addition` = '" . $addition . "'";
+$parties[] = "`typecorrection` = '" . $typecorrection . "'";
+
+$r = "update `ordonnances` set " . implode(", ", $parties) . " where `idordonnance` = '" . $id . "'";
+mysqli_query($con, $r);
+redirection("ordonnances-list.php");
+?>
